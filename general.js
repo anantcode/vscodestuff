97acc0e033d171959196868139191d0ba36b5d04
Anant <anantcode@gmail.com>
2021-08-03 17:34:37 +0530

    General shortcuts

---
+++ general.js
@@ -0,0 +1,27 @@
+/*
+
+Shoutout to Fireship
+
+ 1. Ctrl + Windows + Alt + N        New File
+ 2. @ in command palette            find symbols
+ 3. Ctrl + Shift + .                find symbols
+ 4. Ctrl + D                        find match, multiple lines editing
+ 5. Alt + Up/down                   move line up/down
+ 6. Alt + Shift + Up/down           move line up/down while copying
+ 7. Ctrl + L                        Higlight lines (and keep doing it)
+ 8. Extension: Auto rename tag
+ 9. Ext: Add JSDoc comments
+ 10. new terminal session in pwd    Ctrl + ` (uses default shell)
+ 11. Rename Terminals
+ 12. VSCode Task  (for git)         No need to go to terminal. Run the created task from command palette.
+ 13. Ext: GitLens
+ 14. Ext: Remote Repositories
+ 15. Ext: Remote-SSH
+ 16. Ext: Remote-Containters
+ 17. Custom Snippets (configure user snippets) prebuilt snippets exist as extensions for popular frameworks
+ 18. Shift + Alt + F12              Find all references
+ 19. 
+
+
+
+*
